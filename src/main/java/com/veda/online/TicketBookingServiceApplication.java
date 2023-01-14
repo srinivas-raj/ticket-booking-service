c15bb6506c3e86691dc8336c0790155385fcb7a6
unknown <srinivasa.rajavarapu@gmail.com>
2023-01-14 13:39:46 +0530

    Initial commit

---
+++ src/main/java/com/veda/online/TicketBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.veda.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingServiceApplication.class, args);
+	}
+
+}
